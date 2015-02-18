360866079ae10a9088732a8afaee32b5d2b639b4
wfleurant <williamfleurant@comcast.net>
2015-02-18 00:03:55 -0500

    about: cleanup

--- views/about.php
+++ views/about.php
@@ -33,7 +33,7 @@ $page = 'About';
         </div>
       </div>
     </div>
-<div class="promo">
+    <div class="promo">
       <div class="container">
         <div class="text-center">
           <h1>About Hub</h1>
@@ -42,7 +42,7 @@ $page = 'About';
       </div>
     </div>
     <div class="container" role="main" style="padding-top:40px;">
-      <div class="col-xs-12 col-md-10 col-md-offset-1"> 
+      <div class="col-xs-12 col-md-10 col-md-offset-1">
         <p class="lead">Hub was created by <a href="http://socialno.de/derp">derp</a> in June 2014 as a means to collect and use network information for various Appnode services.</p>
         <p>It was realized the potential value to the public, so we created a public facing API and website with full access to the data. As we move forward with the version 0.1 beta release, we look forward to seeing Hub evolve into a Hyperboria community utility.</p>
       </div>
