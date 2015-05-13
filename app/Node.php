af7d79baacc1a529dd38ea6239b6ab840249d81d
Daniel Supernault <danielsupernault@gmail.com>
2015-05-12 20:33:26 -0600

    Update Node model

--- app/Node.php
+++ app/Node.php
@@ -1,6 +1,7 @@
 <?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Hub\Cjdns\Api;
 
 class Node extends Model {
 
@@ -11,35 +12,79 @@ class Node extends Model {
              */
             protected $table = 'nodes';
 
-            public $timestamps = false;
 
-            protected $fillable = ['public_key'];
+            protected $fillable = [
+            'hostname', 
+            'ownername', 
+            'city', 
+            'province', 
+            'country', 
+            'lat', 
+            'lng',
+            ];
+
+            protected $guarded = [
+            'addr',
+            'public_key',
+            ];
 
             /**
              * The attributes excluded from the model's JSON form.
              *
              * @var array
              */
-            //protected $hidden = ['password', 'remember_token'];
+            protected $hidden = [
+            'privacy_level',
+            ];
 
-            protected $dates = ['first_seen', 'last_seen'];
+            protected $dates = [
+            'first_seen', 
+            'last_seen'
+            ];
+
+            protected $primaryKey = 'addr';
 
 
-            protected function getDateFormat() {
-                return 'Y-m-d\TH:i:sO';
-            }
-            public function setFirstSeenFormattedAttribute($date) {
-                return Carbon::parse($date)->format('c');
-            }
+            public function getCreatedAtAttribute($value) {
 
-            public function setLastSeenFormattedAttribute($date) {
-                return Carbon::parse($date)->format('c');
-            }
-            public function getFirstSeenFormattedAttribute($date) {
-                return Carbon::parse($date)->format('c');
+                return \Carbon\Carbon::parse($value)->toIso8601String();
             }
 
-            public function getLastSeenFormattedAttribute($date) {
-                return Carbon::parse($date)->format('c');
+            public function getUpdatedAtAttribute($value) {
+
+                return \Carbon\Carbon::parse($value)->toIso8601String();
             }
+
+            public function comments() {
+                return $this->hasMany('App\Comment', 'target', 'addr')->orderBy('id', 'DESC')->limit(3);
+            }
+
+            public function peers() {
+                return $this->hasMany('App\Peer', 'origin_ip', 'addr');
+            }
+
+            public function services() {
+                return $this->hasMany('App\Service', 'addr');
+            }
+
+            public function follows() {
+                return $this->hasMany('App\Follower', 'follower_addr', 'addr');
+            }
+
+            public function followers() {
+                return $this->hasMany('App\Follower', 'target', 'addr');
+            }
+
+            public function activity() {
+                return $this->hasMany('App\Activity', 'actor_user_id', 'addr');
+            }
+
+            public function ping($timeout = 200) {
+                return (new Api())->call(
+                    "RouterModule_pingNode",[
+                    "path" => $this->primaryKey,
+                    "timeout" => $timeout
+                    ]);
+            }
+
 }
