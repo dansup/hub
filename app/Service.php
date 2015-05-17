ff8a512800ec1deaa3e5d9c788af8408dbe7c2f3
Daniel Supernault <danielsupernault@gmail.com>
2015-05-16 22:20:39 -0600

    Add service model

--- app/Service.php
+++ app/Service.php
@@ -4,6 +4,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model {
 
-	//
+        protected $table = 'services';
+        protected $fillable = [
+        'name', 
+        'addr',
+        'bio',
+        'city',
+        'country'];
 
+        public function node() {
+        return $this->belongsTo('Node');
+        }
 }
