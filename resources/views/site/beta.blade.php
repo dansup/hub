c82af80e65124995bf1f3d8e4412d045f5e76ab1
Daniel Supernault <danielsupernault@gmail.com>
2015-05-16 22:55:41 -0600

    Remove outdated info from beta template

--- resources/views/site/beta.blade.php
+++ resources/views/site/beta.blade.php
@@ -15,38 +15,6 @@
         </div>
     </div>
 
-    <div class="col-xs-12 col-sm-8 col-sm-offset-2 panel panel-default">
-        <div class="panel-body" style="font-size:17px;">
-            <p class="lead text-center">Known Bugs:</p>
-            <ul>
-                <li><strike>CAPI Cron Collection</strike> - Fixed using closure artisan scheduling</li>
-                <li><strike>IPv6 Padding</strike> - Fixed w/ custom <code>Request::ip()</code></li>
-                <li>Non-unique avatar - need to port code from slim-legacy</li>
-                <li>cjdns bug counts <code>self::public_key</code> as a peer - temporary fix by unsetting first result, should fix on the <code>$node->save()</code> method</li>
-            </ul>
-        </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-8 col-sm-offset-2 panel panel-default">
-        <div class="panel-body" style="font-size:17px;">
-            <p class="lead text-center">Todo:</p>
-            <ul>
-                <li>duplicate pk2ip handler (requires <code>public_key</code> as primary key)</li>
-                <li>Missing foreign keys on <code>App\Node</code> model</li>
-                <li>fix styling on followModal</li>
-                <li>API v1</li>
-                <li>API v1.1 (oauth)</li>
-                <li>nmapper cron, controller, model</li>
-                <li>nodeinfo.json discoverer</li>
-                <li>lucerne ?</li>
-                <li>caching (<code>Cache::</code>)</li>
-                <li>News</li>
-                <li>Maps</li>
-                <li>People</li>
-                <li>Services</li>
-            </ul>
-        </div>
-    </div>
 </div>
 </div>
 
