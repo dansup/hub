86fcc2f9061f7c55220915e7868c88db4981f4dd
Daniel Supernault <danielsupernault@gmail.com>
2015-05-16 22:20:50 -0600

    Update user model

--- app/User.php
+++ app/User.php
@@ -31,4 +31,8 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 */
 	protected $hidden = ['password', 'remember_token'];
 
+	public function nodes() {
+		return $this->belongsToMany('App\Node')->withPivot('addr', 'id');
+	}
+
 }
