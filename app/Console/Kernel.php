9aea869a000cbea6510cf1737a4ac45ba40679dc
Daniel Supernault <danielsupernault@gmail.com>
2015-05-16 22:51:12 -0600

    Add kernel jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,39 @@ class Kernel extends ConsoleKernel {
 	{
 		$schedule->command('inspire')
 				 ->hourly();
+
+		$schedule->call(function() {
+		    $pg = 0;
+		    $api = new \App\Hub\Cjdns\Api;
+		    $nodes = $api->call('NodeStore_dumpTable', ['page'=>$pg]);
+		    $nmax = $nodes['count'] / 4;
+		    for ($i=0; $i < $nmax; $i++) { 
+		        $tnodes = $api->call('NodeStore_dumpTable', ['page' => $i]);
+		        foreach($tnodes['routingTable'] as $n) {
+		            $frag = explode('.', $n['addr']);
+		            $ip = $n['ip'];
+		            $node = \App\Node::firstOrNew([
+		                'public_key' => $frag[5].'.k',
+		                ]);
+		            $node->addr = $n['ip'];
+		            $node->public_key = $frag[5].'.k';
+		            $node->version = substr($frag[0], 1);
+		            $node->touch();
+		            $node->save();
+
+		        }
+		    }
+		    return true;
+		})->everyTenMinutes();
+
+		$schedule->call(function() { 
+			    $nodes = \App\Node::orderByRaw("RAND()")->take(150)->get();
+                foreach ($nodes as $n)
+                    {
+                        (new \App\Http\Controllers\PingController())->apiPing($n->addr);
+                    }
+                return true;
+		})->everyFiveMinutes();
 	}
 
 }
