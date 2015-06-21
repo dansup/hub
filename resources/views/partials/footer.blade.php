b0417eb109e1c2fe9b9a152032d3b682ca0f6189
Daniel Supernault <danielsupernault@gmail.com>
2015-06-21 15:15:47 -0600

    Added Hub Version env variable, and used it in the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -11,38 +11,17 @@
         </ul>
       </div>
       <div class="col-xs-12 col-sm-2">
-        <h3 class="text-center">Help</h3>
-        <ul>
-          <li><a href="/docs">Docs</a></li>
-          <li><strike><a>F.A.Q's</a></strike></li>
-          <li><a href="/site/report/abuse">Report Abuse</a></li>
-          <li><a href="/site/report/bugs">Report Bug</a></li>
-        </ul>
       </div>
       <div class="col-xs-12 col-sm-2">
-        <h3 class="text-center">API</h3>
-        <ul>
-          <li><strike><a>Introduction</a></strike></li>
-          <li><strike><a>Authentication</a></strike></li>
-          <li><strike><a>Endpoints</a></strike></li>
-          <li><strike><a>Examples</a></strike></li>
-        </ul>
       </div>
       <div class="col-xs-12 col-sm-2">
-        <h3 class="text-center">Site</h3>
-        <ul>
-          <li><a href="/site/beta">Beta</a></li>
-          <li><strike><a>Blog</a></strike></li>
-          <li><strike><a>Example Link</a></strike></li>
-          <li><strike><a>Sitemap</a></strike></li>
-        </ul>
       </div>
       <div class="col-xs-12 col-sm-4 credits">
         <p>
-          Hub <a href="https://github.com/dansup/hub">v 0.6</a>
+          Hub <a href="https://github.com/dansup/hub">v {{ env('HUB_VERSION') }}</a>
         </p>
         <p>
-          Created by <a href="http://socialno.de/derp">&#64;dansup</a> &amp; <a href="http://socialno.de/igel">&#64;igel</a>
+          Created by <a href="https://github.com/dansup">&#64;dansup</a> &amp; <a href="https://github.com/wfleurant">&#64;igel</a>
         </p>
       </div>
     </div>
