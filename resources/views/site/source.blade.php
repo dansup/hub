16b3b04c982c6a166a25353d226c4d33e0772097
Daniel Supernault <danielsupernault@gmail.com>
2015-06-21 15:44:54 -0600

    Update site pages
    Prepare the site pages, features and federation pages are not yet
    finished.

--- resources/views/site/source.blade.php
+++ resources/views/site/source.blade.php
@@ -4,9 +4,18 @@
 
 <div class="row">
 <div class="col-xs-12">
-<div class="page-header">
-<h2>Source Code</h2>
-</div>
+    <div class="page-header text-center">
+        <h2>Source</h2>
+    </div>
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 panel panel-default">
+        <div class="panel-body" style="font-size:17px;">
+            
+            <p class="text-center">
+              <i class="fa fa-code fa-2x"></i> &nbsp; Source code available on <a href="https://github.com/dansup/hub">github</a>.
+            </p>
+
+        </div>
+    </div>
 </div>
 </div>
 
