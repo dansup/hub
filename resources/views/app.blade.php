f3e92f31abee61587a66d683bc05bf9fd39b700a
Daniel Supernault <danielsupernault@gmail.com>
2015-06-28 22:55:55 -0600

    Add updated node listing using bootcards

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
 	<link href="/assets/css/fonts.css" rel="stylesheet">
 	<link href="/assets/css/font-awesome.css" rel="stylesheet">
 	<link href="/assets/css/bootstrap.css" rel="stylesheet">
+	<link href="/assets/css/bootcards-desktop.css" rel="stylesheet">
 	<link href="/assets/css/app.css" rel="stylesheet">
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
@@ -22,6 +23,7 @@
 	<script src="/assets/js/bootstrap.min.js"></script>
 	<script src="/assets/js/jquery.autocomplete.min.js"></script>
 	<script src="/assets/js/jquery.timeago.min.js"></script>
+	<script src="/assets/js/bootcards.js"></script>
 	<script src="/assets/js/app.js"></script>
 	@yield('subjs')
 </body>
--- resources/views/node/home.blade.php
+++ resources/views/node/home.blade.php
@@ -1,55 +1,55 @@
 @extends('app')
 
 @section('content')
-  @if($errors->has())
-    <div class="col-xs-12 col-sm-10 col-sm-offset-1">
-     @foreach ($errors->all() as $error)
-      <div class="alert alert-info">
-        <p class="alert-text lead">{{ $error }}</p>
+@if($errors->has())
+<div class="col-xs-12 col-sm-10 col-sm-offset-1">
+  @foreach ($errors->all() as $error)
+  <div class="alert alert-info">
+    <p class="alert-text lead">{{ $error }}</p>
+  </div>
+  @endforeach
+</div>
+@endif
+<div class="col-xs-12 col-md-10 col-md-offset-1">
+  <div class="col-xs-12 col-sm-8">
+    {!! $nodes->render() !!}
+  </div>
+  <div class="col-xs-12 col-sm-4">
+    <p class="text-right">
+      <a class="btn btn-default" href="/nodes/me">My Node</a>
+      <a class="btn btn-default" href="/nodes/create">Add Node</a>
+    </p>
+  </div>
+  <div class="col-xs-12">
+
+    <div class="bootcards-list">
+      <div class="panel panel-default">
+        <div class="list-group">
+          @foreach ($nodes as $n)
+
+          <a class="list-group-item" href="/nodes/{{$n->addr}}">
+            <img src="{!! ($n->avatar_hash == null) ? '/assets/img/avatar.png' : '/assets/avatars/'.$n->avatar_hash !!}" alt="{{$n->addr}}'s node avatar" class="img-rounded pull-left" width="40px"/>
+            <h4 class="list-group-item-heading">{{ $n->addr }}</h4>
+            <p class="list-group-item-text">
+              <small>
+                {{$n->public_key}}
+              </small> | 
+              <span class="badge">
+                v {{ $n->version }}
+              </span> | 
+              <span class="badge">
+                seen <time class="timeago" datetime="{{$n->updated_at}}"></time>
+              </span>
+            </p>
+          </a>
+
+          @endforeach
+        </div>
       </div>
-    @endforeach
     </div>
-  @endif
-  <div class="col-xs-12 col-md-10 col-md-offset-1">
-    <div class="col-xs-12 col-sm-8">
-      <?= $nodes->render() ?>
-    </div>
-    <div class="col-xs-12 col-sm-4">
-     <p class="text-right">
-     <a class="btn btn-default" href="/nodes/me">My Node</a>
-     <a class="btn btn-default" href="/nodes/create">Add Node</a>
-     </p>
-    </div>
-    <div class="alert alert-info" style="display:block;margin-top:60px;">
-      <p class="alert-text">View <a href="http://dev.hub.hyperboria.net/nodes/fcec:ae97:8902:d810:6c92:ec67:efb2:3ec5" style="font-weight:600;">an example</a> of a node w/ activity</p>
-    </div>
-  <table class="table table-bordered table-striped">
-      <thead>
-        <tr>
-          <th>IPv6 Address</th>
-          <th>Hostname</th>
-          <th>First Seen</th>
-          <th>Last Seen</th>
-          <th>Latency</th>
-          <th>Version</th>
-        </tr>
-      </thead>
-      <tbody>
-    <?php foreach ($nodes as $n): ?>
-        <tr>
-          <th scope="row"><a href="/nodes/{{{ $n->addr }}}" class="ipv6">{{{ $n->addr }}}</a></th>
-          <td>{{{ $n->hostname }}}</td>
-          <td><time class="timeago" datetime="{{{ $n->created_at }}}">{{{ $n->created_at }}}</time></td>
-          <td><time class="timeago" datetime="{{{ $n->updated_at }}}">{{{ $n->updated_at }}}</time></td>
-          <td><strong>{{{ $n->latency }}}</strong> ms</td>
-          <td>v<strong>{{{ $n->version }}}</strong></td>
-        </tr>
-        <tr>
-    <?php endforeach; ?>
-      
-      </tbody>
-    </table>
-    <?= $nodes->render() ?>
+
+    {!! $nodes->render() !!}
+  </div>
 </div>
 
-@endsection
+  @endsection
