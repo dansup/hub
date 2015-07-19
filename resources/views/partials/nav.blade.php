d2b36d78ddb9b2d4df0c1ebe373faeb554d3011c
Daniel Supernault <danielsupernault@gmail.com>
2015-07-18 19:58:42 -0600

    Update navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -12,8 +12,10 @@
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-            <li><a href="{{ url('/nodes') }}">Nodes</a></li>
-            <li><a href="{{ url('/services') }}">Services</a></li>
+            <li><a href="/"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="{{ url('/nodes') }}"><i class="fa fa-th"></i> Nodes</a></li>
+            <li><a href="{{ url('/services') }}"><i class="fa fa-bookmark"></i> Services</a></li>
+            <li><a href="{{ url('/people') }}"><i class="fa fa-user"></i> People</a></li>
             <form class="navbar-form navbar-right">
             <input type="text" id="autocomplete" class="form-control" placeholder="Search...">
             <div class="autocomplete-suggestions">
@@ -22,8 +24,8 @@
         </ul>
         <ul class="nav navbar-nav navbar-right">
             @if (Auth::guest())
-{{--             <li><a href="{{ url('/auth/login') }}">Login</a></li>
-            <li><a href="{{ url('/auth/register') }}">Register</a></li> --}}
+            <li><a href="{{ url('/auth/login') }}">Login</a></li>
+            <li><a href="{{ url('/auth/register') }}">Register</a></li>
             @else
             <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
