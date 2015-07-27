956c8d38c4e89f2b2e3e4634c5ec653677c87349
wfleurant <williamfleurant@comcast.net>
2015-07-26 20:08:46 -0400

    nodestats: tidy the view, add pretty bytes, still example.

--- resources/views/node/nodestats.blade.php
+++ resources/views/node/nodestats.blade.php
@@ -27,7 +27,7 @@
                 <th>pubkey</th>
                 <th>bytesin</th>
                 <th>bytesout</th>
-                <th>version</th>
+                <!-- <th>version</th> -->
               </tr>
             </thead>
           </table>
@@ -43,53 +43,64 @@
 
 @section('subjs')
 
-<!-- <link rel="stylesheet" href="/assets/css/jquery.dataTables.min.css"> -->
+<link rel="stylesheet" href="/assets/css/jquery.dataTables.min.css">
 <script src="/assets/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript">
 
   jQuery(document).ready(function($) {
 
-    jQuery.getJSON(
-     '/api/v0/node/{{{ $n->addr }}}/peers.json',
-     { node: '{{{ $n->addr }}}' }, function(json, textStatus) {
-      var dataSet = json;
-      var textSet = textStatus;
-      console.log('dataSet -> %s', dataSet);
-      console.log('textSet -> %s', textSet);
-    });
+    function prettyBytes(num) {
+        if (typeof num !== 'number') {
+          throw new TypeError('Expected a number');
+        }
+        var exponent;
+        var unit;
+        var neg = num < 0;
+        var units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        if (neg) {
+          num = -num;
+        }
+
+        if (num < 1) {
+          return (neg ? '-' : '') + num + ' B';
+        }
+
+        exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
+        num = (num / Math.pow(1000, exponent)).toFixed(2) * 1;
+        unit = units[exponent];
+
+        return (neg ? '-' : '') + num + ' ' + unit;
+      };
 
 
     /* See: ApiController::getNodePeers() */
     $('#nodestats-table').dataTable( {
-        // "data": dataSet
+        // "ajax": '/api/v0/node/{{{ $n->addr }}}/peers.json',
         "data": [
                   [
                     "ESTABLISHED",
                     "vf6src4q8v26ru97q4f6bprmldsd0c08vbuttb04bcyf94xljxu0.k",
-                    "28655924",
-                    "39993288",
-                    "v16"
+                    prettyBytes(28655924),
+                    prettyBytes(39993288),
                   ],
                   [
                     "ESTABLISHED",
                     "lqlg9mfppn8vs5ztx2fr9x9gvw2d7g9p885v7tsucn8zz2rdukw0.k",
-                    "5128520",
-                    "656646",
-                    "v16",
+                    prettyBytes(5128520),
+                    prettyBytes(656646),
                   ],
                   [
                     "ESTABLISHED",
                     "2hwmvzgxhy6h02zsk81cz2h4ybwh1hxjtsk7j8cu9954lzwgu8t0.k",
-                    "17164521",
-                    "925119",
-                    "v16",
+                    prettyBytes(17164521),
+                    prettyBytes(925119),
                   ],
                   [
                     "ESTABLISHED",
                     "xrj4g8klznc6ju2q1ljktlhff08c24lglmyqwtddtjy3vlsgrwq0.k",
-                    "4848688",
-                    "393939",
-                    "v16",
+                    prettyBytes(4848688),
+                    prettyBytes(393939),
                   ],
                 ]
     });
