9ddd1eb16c9f679f8eaf554f416b7288204c2cd4
wfleurant <williamfleurant@comcast.net>
2015-07-26 23:54:22 -0400

    nodestats: nodestats blade ajax ok, still docker ip hard coded

--- resources/views/node/nodestats.blade.php
+++ resources/views/node/nodestats.blade.php
@@ -5,13 +5,11 @@
   <div role="tabpanel">
     <div class="col-md-3">
       @include('node.partials.sidebar-nav', [ 'ip' => $n->addr ])
-      <!-- Missing $avatar_hash and public key -->
     </div>
     <div class="col-md-9">
       @include('node.partials.content-header')
-      <!-- Missing $public_key -->
-
       <div class="profile-content active">
+
         <div class="col-xs-12 col-md-8 col-md-offset-2 text-center">
           <div class="page-header text-center">
             <h2>Node Stats <span class="text-muted">(Example)</span></h2>
@@ -19,15 +17,12 @@
         </div>
 
         <div class="col-xs-12 col-md-8 col-md-offset-0 text-center">
-
           <table id='nodestats-table' class="table table-hover">
             <thead>
               <tr>
-                <th>state</th>
-                <th>pubkey</th>
-                <th>bytesin</th>
-                <th>bytesout</th>
-                <!-- <th>version</th> -->
+              @foreach ($tophubcol as $value)
+                <th>{{ $value }}</th>
+              @endforeach
               </tr>
             </thead>
           </table>
@@ -40,74 +35,53 @@
 
 @endsection
 
-
 @section('subjs')
-
 <link rel="stylesheet" href="/assets/css/jquery.dataTables.min.css">
 <script src="/assets/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript">
 
+<script type="text/javascript">
   jQuery(document).ready(function($) {
 
-    function prettyBytes(num) {
-        if (typeof num !== 'number') {
-          throw new TypeError('Expected a number');
-        }
-        var exponent;
-        var unit;
-        var neg = num < 0;
-        var units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var peers = jQuery.getJSON(
+      '/api/v0/node/{{{ $n->addr }}}/peers.json', { node: '{{{ $n->addr }}}'}
+    );
 
-        if (neg) {
-          num = -num;
-        }
+    peers.done(function(peers) {
 
-        if (num < 1) {
-          return (neg ? '-' : '') + num + ' B';
-        }
+      console.table(peers);
 
-        exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
-        num = (num / Math.pow(1000, exponent)).toFixed(2) * 1;
-        unit = units[exponent];
+      $('#nodestats-table').dataTable( {
 
-        return (neg ? '-' : '') + num + ' ' + unit;
-      };
+        "data": peers,
 
+        @foreach ($falseopts as $value)
+        {{ "$value" }}: false,
+        @endforeach
 
-    /* See: ApiController::getNodePeers() */
-    $('#nodestats-table').dataTable( {
-        // "ajax": '/api/v0/node/{{{ $n->addr }}}/peers.json',
-        "data": [
-                  [
-                    "ESTABLISHED",
-                    "vf6src4q8v26ru97q4f6bprmldsd0c08vbuttb04bcyf94xljxu0.k",
-                    prettyBytes(28655924),
-                    prettyBytes(39993288),
-                  ],
-                  [
-                    "ESTABLISHED",
-                    "lqlg9mfppn8vs5ztx2fr9x9gvw2d7g9p885v7tsucn8zz2rdukw0.k",
-                    prettyBytes(5128520),
-                    prettyBytes(656646),
-                  ],
-                  [
-                    "ESTABLISHED",
-                    "2hwmvzgxhy6h02zsk81cz2h4ybwh1hxjtsk7j8cu9954lzwgu8t0.k",
-                    prettyBytes(17164521),
-                    prettyBytes(925119),
-                  ],
-                  [
-                    "ESTABLISHED",
-                    "xrj4g8klznc6ju2q1ljktlhff08c24lglmyqwtddtjy3vlsgrwq0.k",
-                    prettyBytes(4848688),
-                    prettyBytes(393939),
-                  ],
-                ]
+        "columns": [
+          @foreach ($tophubcol as $value)
+            { "data": "{{ $value }}" },
+          @endforeach
+        ]
+      });
     });
 
-
 });
+</script>
 
+<script type="text/javascript">
+
+  function prettyBytes(num) {
+    if (typeof num !== 'number') { throw new TypeError('Expected a number'); }
+    var exponent; var unit; var neg = num < 0;
+    var units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    if (neg) { num = -num; }
+    if (num < 1) { return (neg ? '-' : '') + num + ' B'; }
+    exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1);
+    num      = (num / Math.pow(1000, exponent)).toFixed(2) * 1;
+    unit     = units[exponent];
+    return (neg ? '-' : '') + num + ' ' + unit;
+  };
 </script>
 
 @stop
