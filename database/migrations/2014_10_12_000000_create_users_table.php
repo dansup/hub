81f8fdd6122f99f67c00f69982e4c88b854db7f3
Daniel Supernault <danielsupernault@gmail.com>
2016-02-06 18:19:10 -0700

    Add Laravel 5.2 Framework

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('ipv6')->unique();
+            $table->string('email')->unique();
+            $table->enum('auth_type', ['password','ipv6'])->default('password');
+            $table->boolean('valid_email')->default(false);
+            $table->string('password', 60)->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
